7af4b144ea8a3cbe3d172f1db5bb6337f14c16a1
Alainsol <85121697+Alainsol@users.noreply.github.com>
2021-07-18 19:34:02 +0700

    u

--- lesson-5/js/prophets.js
+++ lesson-5/js/prophets.js
@@ -1,20 +1,36 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
-
-/* Display on the HTML */
-for (let i = 0; i < towns.length; i++ ) {
-    const cityName = towns[i].name;
-    const requestURL = 'api.openweathermap.org/data/2.5/weather?q='+ cityName +',Idaho&appid=910d455d37252cea79740dddb96bcf43';
-    /* Download the data */
-    fetch(requestURL)
+/* Download the data */
+fetch(requestURL)
     .then(function (response) {
         return response.json();
-    })
+      })
     .then(function (jsonObject) {
+
         /* Downloaded data */
-        const weatherData = jsonObject;
-        console.log(weatherData);
-    });  
-}
+        const towns = jsonObject['towns'];
+
+        /* Get town weather data */
+        for (let i = 0; i < towns.length; i++ ) {
+            const cityName = towns[i].name;
+            const requestURL = 'api.openweathermap.org/data/2.5/weather?q='+ cityName +',Idaho&appid=910d455d37252cea79740dddb96bcf43';
+            /* Download the data */
+            fetch(requestURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (jsonObject) {
+                /* Downloaded data */
+                const weatherData = jsonObject;
+                console.log(weatherData);
+            });  
+        }
+
+      });
+
+    
+
+
 
 
 
