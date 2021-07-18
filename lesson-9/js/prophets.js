05008364af506309c93ff0fd946cf494ff92c4e9
Alainsol <85121697+Alainsol@users.noreply.github.com>
2021-07-18 17:02:06 +0700

    updatedfooter

---
+++ lesson-9/js/prophets.js
@@ -0,0 +1,30 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+/* Download the data */
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (jsonObject) {
+
+        /* Downloaded data */
+        const prophets = jsonObject['prophets'];
+
+        /* Display on the HTML */
+        for (let i = 0; i < prophets.length; i++ ) {
+
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            
+            image.setAttribute('src', prophets[i].imageurl);
+
+            card.appendChild(h2);            
+            card.appendChild(image);
+
+            document.querySelector('div.cards').appendChild(card);
+        }
+
+      });
+
+    
