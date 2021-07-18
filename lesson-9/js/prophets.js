6e87a8fc5f29f9ea87004c8d886a254de909c18f
Alainsol <85121697+Alainsol@users.noreply.github.com>
2021-07-18 17:40:13 +0700

    updated in=mage

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -14,13 +14,25 @@ fetch(requestURL)
         for (let i = 0; i < prophets.length; i++ ) {
 
             let card = document.createElement('section');
-            let h2 = document.createElement('h2');
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            let title = document.createElement('h2');
+            title.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+
+            let birthdate = document.createElement("p");
+            dateOfBirth.textContent = prophets[i].birthdate;
+
+            let birthplace  = document.createElement("p");
+            birthplace.textContent = prophets[i].birthplace;
+
+            let order = document.createElement("p");
+            orderrank.textContent = prophets[i].order;
+
+        
             
             let image = document.createElement('img');
+            
             image.setAttribute('src', prophets[i].imageurl);
 
-            card.appendChild(h2);            
+            card.appendChild(title);            
             card.appendChild(image);
 
             document.querySelector('div.cards').appendChild(card);
